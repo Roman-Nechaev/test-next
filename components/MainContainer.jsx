a8eb1e71a8ba0fe1f062ac3d9566c91f22ba945c
Roman-Nechaev <nrchaevroman@gmail.com>
2023-08-17 18:08:28 +0300

    add

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { NavBar } from "../pages/index.styled";
+
 import A from "./A";
 
 const MainContainer = ({ children, keywords }) => {
@@ -9,10 +9,10 @@ const MainContainer = ({ children, keywords }) => {
         <meta keywords={"studying next" + keywords} />
         <title>Главная</title>
       </Head>
-      <NavBar>
+      <div>
         <A href="/" text="Главная" />
         <A href="/users" text="Пользователи" />
-      </NavBar>
+      </div>
       <div>{children}</div>
     </>
   );
