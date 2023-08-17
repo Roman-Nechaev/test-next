32a5b781f2d73c3eefa5876be11b076a18cd6a46
Roman-Nechaev <nrchaevroman@gmail.com>
2023-08-17 18:10:09 +0300

    add

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { NavBar } from "./index.styled";
+
 import A from "../components/A";
 import Head from "next/head";
 import MainContainer from "../components/MainContainer";
